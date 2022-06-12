a7af3cc1ff9553bb6b81c2d022234fac35c44214
Yurii <94929434+Yurii-Sapsai@users.noreply.github.com>
2022-06-12 11:35:06 +0300

    update categories filter

--- src/components/Posts/PostCard/PostCard.jsx
+++ src/components/Posts/PostCard/PostCard.jsx
@@ -1,34 +1,31 @@
-import React, { useEffect, useState } from 'react'
+import React, {useState, useEffect} from 'react'
 import './PostCard.sass'
 
 import { getStorage, ref, getDownloadURL} from "firebase/storage";
 
 
-function PostCard({post}) {
 
-    console.log(post)
+function PostCard({ post }) {
 
-    const {title, text, date, uuid} = post
-
+    const {title, text, uuid, date} = post
     const [url, setUrl] = useState('')
 
     const storage = getStorage();
-
     useEffect(()=>{
         getDownloadURL(ref(storage, `gs://blog-app-7d4ac.appspot.com/${uuid}`))
         .then((url) => setUrl(url))
-    },[])
-    
+    },[title])
+
     return (
         <div className='postCard'>
 
             <h1><mark>{title}</mark></h1>
             <img src={url} alt="img" />
             <p>{text}</p>
-            <span>{date}</span> 
+            <span>{date}</span>
 
         </div>
     )
 }
 
-export default PostCard
+export default PostCard 
--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -4,53 +4,52 @@ import './Posts.sass'
 import PostCard from './PostCard/PostCard';
 
 import { getDatabase, ref, child, get } from "firebase/database";
-import {useSelector} from 'react-redux'
+
+import { useSelector } from 'react-redux'
 
 function Posts() {
 
-    const [posts, setPosts] = useState({})
-    const {category} = useSelector(state => state.category)
+    const [posts, setPosts] = useState([])
+    const { category } = useSelector(state => state.category)
 
     const dbRef = ref(getDatabase());
 
-    useEffect(()=>{
+    useEffect(() => {
 
         get(child(dbRef, `posts/`))
-            .then((snapshot) => {
-            if (snapshot.exists()) {
-                setPosts(snapshot.val())
-            } else {
-                console.log("No data available");
-            }
+            .then((item) => {
+                if (item.exists()) {
+
+                    let newArr = []
+                    for (let key in item.val()) {
+                        newArr.push(item.val()[key])
+                    }
+
+                    if (category) {
+                        newArr = newArr.filter((item) => item.category.includes(category))
+                    }
+
+                    setPosts(newArr)
+                } else {
+                    console.log("No data available");
+                }
             }).catch((error) => {
-            console.error(error);
+                console.error(error);
             });
-            
-    },[])
+    }, [category])
 
-   useEffect(()=>{
-    let newArr = []
-    for(let key in posts){
-       newArr.push(posts[key])
-    }
-       const filteredPosts = newArr.filter((item) => item.category.includes(category)); 
-       console.log(filteredPosts)
-      setPosts(filteredPosts); 
-    },[category]) 
 
-    let newArr = []
-    for(let key in posts){
-       newArr.push(posts[key])
-    }
-    console.log(newArr)
+    return (
+        <div className='posts' >
+            {posts.map((post, index) => (
 
-  return (
-    <div className='posts' >
-        {newArr.map((post, index)=>(
-            <PostCard post={post} key={index}/>
-        ))}
-    </div>
-  )
+                <PostCard post={post} key={index} />
+
+            ))}
+        </div>
+    )
 }
 
 export default Posts
+
+
